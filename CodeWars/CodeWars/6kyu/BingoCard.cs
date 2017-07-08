93713477ebd82d00877452e45b08b25bc34c0df2
YiChi Lin <sky61054@gmail.com>
2017-07-08 16:02:26 +0800

    Added BingoCard class and test class.

---
+++ CodeWars/CodeWars/6kyu/BingoCard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CodeWars._6kyu
+{
+    internal class BingoCard
+    {
+        public IEnumerable<string> GetCards()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CodeWars/CodeWarsTests/6ku/BingoCardTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using CodeWars._6kyu;
+using NUnit.Framework;
+
+namespace CodeWarsTests._6ku
+{
+    [TestFixture]
+    public class BingoCardTest
+    {
+        [Test]
+        public void Has24Cards()
+        {
+            var target = new BingoCard();
+            var result = target.GetCards();
+            Assert.AreEqual(24,result.Count());
+        }
+    }
+}
