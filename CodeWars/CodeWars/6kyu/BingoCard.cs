5401071b9f8b1593d545b55c1d3dd746cadb99e4
YiChi Lin <sky61054@gmail.com>
2017-07-08 17:26:50 +0800

    Passed first test

--- CodeWars/CodeWars/6kyu/BingoCard.cs
+++ CodeWars/CodeWars/6kyu/BingoCard.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace CodeWars._6kyu
 {
     internal class BingoCard
     {
+
         public IEnumerable<string> GetCards()
         {
-            throw new System.NotImplementedException();
+            var cards = new List<string>();
+            var random = new Random();
+
+            for (var index = 0; index < 24; index++)
+            {
+                cards.Add(random.Next(1,75).ToString());
+            }
+
+            return cards;
         }
     }
 }
