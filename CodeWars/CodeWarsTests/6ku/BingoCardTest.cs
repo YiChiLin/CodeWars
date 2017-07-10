1b7aa2103094101d02816ef0c55e152af09ec9eb
YiChi Lin <sky61054@gmail.com>
2017-07-10 10:57:52 +0800

    added RandomnessTest

--- CodeWars/CodeWarsTests/6ku/BingoCardTest.cs
+++ CodeWars/CodeWarsTests/6ku/BingoCardTest.cs
@@ -22,5 +22,26 @@ namespace CodeWarsTests._6ku
             var result = target.GetCards();
             Assert.AreEqual(result.Count(), result.Distinct().Count());
         }
+
+        [Test]
+        public void RandomnessTest()
+        {
+            var target = new BingoCard().GetCards().ToArray();
+            var duplicateCardNumberCount = (new int[24]).ToList();
+
+            for (var cardSet = 0; cardSet < 100; cardSet++)
+            {
+                var randomCard = new BingoCard().GetCards().ToArray();
+                for (var j = 0; j < 24; j++)
+                {
+                    if (target[j] == randomCard[j])
+                    {
+                        duplicateCardNumberCount[j]++;
+                    }
+                }
+            }
+
+            Assert.IsFalse(duplicateCardNumberCount.Any(x => x > 30), "The same number appeared on more than 30 of the 100 cards on the same spot, are the cards random?");
+        }
     }
 }
