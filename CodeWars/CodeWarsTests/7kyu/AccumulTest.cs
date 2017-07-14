1a541686d3f9928d9d87995733c98683c40641e0
YiChi Lin <sky61054@gmail.com>
2017-07-14 14:27:28 +0800

    Removed unused file and add new test case for accumulate

---
+++ CodeWars/CodeWarsTests/7kyu/AccumulTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace CodeWarsTests._7kyu
+{
+    [TestFixture]
+    public class AccumulTest
+    {
+        [Test]
+        public void Accumlate_A_and_Will_Get_A()
+        {
+            var target = new Accumul();
+            var result = target.Accum("A");
+            
+        }
+    }
+
+    internal class Accumul
+    {
+        public string Accum(string str)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
