594c9489a0c3c5ba8b756de0d8ee33f731b012b4
YiChi Lin <sky61054@gmail.com>
2017-07-17 13:56:28 +0800

    Passed TestCase

--- CodeWars/CodeWarsTests/7kyu/AccumulTest.cs
+++ CodeWars/CodeWarsTests/7kyu/AccumulTest.cs
@@ -1,16 +1,37 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
 
 namespace CodeWarsTests._7kyu
 {
     [TestFixture]
     public class AccumulTest
     {
+        //Mumbling
         [Test]
         public void Accumlate_A_and_Will_Get_A()
         {
             var target = new Accumul();
             var result = target.Accum("A");
-            
+            Assert.AreEqual("A",result);
+        }
+
+        [TestCase("AB","A-Bb")]
+        public void Accumlate(string input,string expected)
+        {
+            var target = new Accumul();
+            var result = target.Accum(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [Ignore]
+        public void testFun()
+        {
+            var str = "Apfel";
+            var strLength = str.Count();
+            var separateArr = str.ToCharArray();
         }
     }
 
@@ -18,7 +39,23 @@ namespace CodeWarsTests._7kyu
     {
         public string Accum(string str)
         {
-            throw new System.NotImplementedException();
+            var resultStr = new StringBuilder();
+            
+            var separateArr = str.ToCharArray();
+            for (var i = 0; i < separateArr.Length; i++)
+            {
+                resultStr.Append(separateArr[i].ToString().ToUpper());
+                for (var a = 1; a <= i; a++)
+                {
+                    resultStr.Append(separateArr[i].ToString().ToLower());
+                }
+                if (i != separateArr.Length-1)
+                {
+                    resultStr.Append("-");                    
+                }
+            }
+
+            return resultStr.ToString();
         }
     }
 }
