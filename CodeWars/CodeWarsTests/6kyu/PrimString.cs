48cf6a2225249b0c66230658f1279c231db0c7a9
YiChi Lin <sky61054@gmail.com>
2017-07-23 15:31:59 +0800

    Added Prime String test

---
+++ CodeWars/CodeWarsTests/6kyu/PrimString.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace CodeWarsTests._6kyu
+{
+    [TestFixture]
+    public class PrimString
+    {
+        
+        [Test]
+        public void Give_abcd_should_be_true()
+        {
+            var target = new PrimeString();
+            var result = target.IsPrimeString("abcd");
+
+            Assert.AreEqual(true, result);
+        }
+    }
+
+    internal class PrimeString
+    {
+        public bool IsPrimeString(string str)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
