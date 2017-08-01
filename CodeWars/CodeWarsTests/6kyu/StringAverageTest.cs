119167a66c71e220da94ee88fc2d41d2be776760
YiChi Lin <sky61054@gmail.com>
2017-08-01 21:59:54 +0800

    Implement production code and pass test case

--- CodeWars/CodeWarsTests/6kyu/StringAverageTest.cs
+++ CodeWars/CodeWarsTests/6kyu/StringAverageTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 
 namespace CodeWarsTests._6kyu
 {
@@ -12,13 +14,34 @@ namespace CodeWarsTests._6kyu
             var result = target.GetAvarage(input);
             Assert.AreEqual(expected, result);
         }
+
+        
     }
 
     public class StringAverage
     {
+        private readonly Dictionary<object, object> _numberStringConverter = new Dictionary<object, object>
+        {
+            {"zero", 0},{"one", 1},{"two", 2},{"three", 3},{"four", 4},{"five", 5},{"six", 6},{"seven", 7},{"eight", 8},{"nine", 9},
+            {0, "zero"},{1, "one"},{2, "two"},{3, "three"},{4, "four"},{5, "five"},{6, "six"},{7, "seven"},{8, "eight"},{9, "nine"},
+        };
+
+
         public string GetAvarage(string input)
         {
-            throw new System.NotImplementedException();
+            var inputNumbers = input.Split(' ');
+            var avarage = GetSum(inputNumbers) / inputNumbers.Count();
+            return NumberStringConverter(avarage).ToString();
+        }
+
+        private int GetSum(string[] inputNumbers)
+        {
+            return inputNumbers.Select(value => (int)NumberStringConverter(value)).Sum();
+        }
+
+        private object NumberStringConverter(object key)
+        {
+            return _numberStringConverter[key];
         }
     }
 }
