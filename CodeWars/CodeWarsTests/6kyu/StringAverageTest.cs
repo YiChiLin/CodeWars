f8b3aa0a0cfe374c24af2185b871edf3f8afb66d
YiChi Lin <sky61054@gmail.com>
2017-08-01 17:47:46 +0800

    Added string avarage kata test

---
+++ CodeWars/CodeWarsTests/6kyu/StringAverageTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace CodeWarsTests._6kyu
+{
+    [TestFixture]
+    public class StringAverageTest
+    {
+        [TestCase("zero nine five two","four")]
+        public void NormalCaseTest(string input,string expected)
+        {
+            var target = new StringAverage();
+            var result = target.GetAvarage(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+
+    public class StringAverage
+    {
+        public string GetAvarage(string input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
