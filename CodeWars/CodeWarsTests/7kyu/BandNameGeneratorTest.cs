7d7fb3feb6ced477ac6de8364a702db2382cf49b
YiChi Lin <sky61054@gmail.com>
2017-08-13 11:38:48 +0800

    Added Band Name Generator test

---
+++ CodeWars/CodeWarsTests/7kyu/BandNameGeneratorTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace CodeWarsTests._7kyu
+{
+    [TestFixture]
+    public class BandNameGeneratorTest
+    {
+        [Test]
+        public void BandName_dolphin_ShouldBe_The_Dolphin()
+        {
+            var target = new BandNameGenerator();
+            var actual = target.GetBandName("dolphin");
+            Assert.AreEqual("The Dolphin", actual);
+        }
+    }
+
+    public class BandNameGenerator
+    {
+        public string GetBandName(string originalName)
+        {
+            var firstLetter = originalName[0].ToString().ToUpper();
+            var formattedName = "The " + firstLetter + originalName.Substring(1);
+            return formattedName;
+        }
+    }
+}
