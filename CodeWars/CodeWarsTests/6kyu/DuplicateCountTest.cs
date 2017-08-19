5b2a02d8ffeb80b0e5725641ee8c6e192e664d02
Jean Lin Yi Chi <jean.lin52@titansoft.com.sg>
2017-08-19 10:58:43 +0800

    Added DuplicateCountTest and implement first test case

---
+++ CodeWars/CodeWarsTests/6kyu/DuplicateCountTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CodeWarsTests._6kyu
+{
+    [TestFixture]
+    public class DuplicateCountTest
+    {
+        [TestCase("AB",0,TestName = "CapitalizeInput_AB_CountIs_0")]
+        public void Test(string input,int expected)
+        {
+            var target = new DuplicateCount();
+            var actual = target.GetCount(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class DuplicateCount
+    {
+        public int GetCount(string input)
+        {
+            input = input.ToLower();
+            var record = new Dictionary<char,int>();
+
+            foreach (var character in input)
+            {
+                if (record.ContainsKey(character))
+                    record[character] += 1;
+                else
+                    record.Add(character, 1);
+            }
+
+            return record.Count(x => x.Value > 1);
+        }
+    }
+}
