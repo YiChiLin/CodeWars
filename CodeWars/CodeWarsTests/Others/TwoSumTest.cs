0612f736b1bc3074c0f6d0497bcf4b1d45d7faf7
jean.lin52 <jean.lin52@TI03BY32>
2017-09-10 16:28:05 +0800

    Added TwoSum

---
+++ CodeWars/CodeWarsTests/Others/TwoSumTest.cs
@@ -0,0 +1,35 @@
+﻿using ExpectedObjects;
+using NUnit.Framework;
+
+namespace CodeWarsTests.Others
+{
+    [TestFixture]
+    public class TwoSumTest
+    {
+        [TestCase(9, new int[] { 2, 7, 11, 5 })]
+        public void Test(int sum, params int[] input)
+        {
+            var target = new TwoSum();
+            var actual = target.GetSum(input,sum);
+            var expected = new int[] { 0, 1 };
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+
+    internal class TwoSum
+    {
+        internal int[] GetSum(int[] input,int expected)
+        {
+            for(var i = 0; i < input.Length; i++)
+            {
+                if( i+1 < input.Length)
+                {
+                    var sum = input[i] + input[i + 1];
+                    if (sum == expected)
+                        return new int[] { i, i + 1 };
+                }
+            }
+            return new int[] { 0 };
+        }
+    }
+}
