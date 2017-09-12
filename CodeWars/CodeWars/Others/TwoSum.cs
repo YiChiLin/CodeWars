c94439900c7651682c0e18e0c6aeb7034a57c899
jean.lin52 <jean.lin52@TI03BY32>
2017-09-12 19:01:37 +0800

    Added TwoSum

---
+++ CodeWars/CodeWars/Others/TwoSum.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWars.Others
+{
+    internal class TwoSum
+    {
+        internal int[] GetSum(int[] input,int expected)
+        {
+            for(var i = 0; i < input.Length; i++)
+            {
+                if( i+1 < input.Length)
+                {
+                    var sum = input[i] + input[i + 1];
+                    if (sum == expected)
+                        return new[] { i, i + 1 };
+                }
+            }
+            return new[] {0, 0};
+        }
+    }
+}
