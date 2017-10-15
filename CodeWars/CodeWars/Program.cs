089b4cb27f99d1db69ad3094bc5ae6ba1e2c2df2
jean.lin52 <jean.lin52@TI03BY32>
2017-10-15 15:51:43 +0800

    added string summarion utility

---
+++ CodeWars/CodeWars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CodeWars.Utility;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "This is a long long long long long long long long long long long long story.";
+            var summary = StringUtility.SummarisingText(text, 10);
+            Console.WriteLine(summary);
+        }
+    }
+}
