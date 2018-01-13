7b17929e2aefd74c205ac2a061eb6186bcd83c4d
jean.lin52 <jean.lin52@TI03BY32>
2018-01-13 11:02:47 +0800

    added

---
+++ CodeWars/CodeWarConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeWarConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Dictionary<int, int> {
+                { 1,1}, { 2,-1}, { 3,1}, { 4,-1},
+                { 5,1}, { 6,-1}, { 7,1}, { 8,1},
+                { 9,1}
+            };
+
+            var winningOrderCount = 0;
+            var set = 0;
+            foreach (var item in data)
+            {
+                if (item.Value > 0)
+                {
+                    winningOrderCount++;
+                    if (winningOrderCount == 3)
+                    {
+                        set++;
+                        winningOrderCount = 0;
+                    }
+                }
+                else
+                {
+                    winningOrderCount = 0;
+                }
+            }
+
+            Console.WriteLine($"Set = {set}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodeWars/CodeWars/Practice.cs
@@ -0,0 +1,52 @@
+﻿namespace CodeWars
+{
+    public interface ILife
+    {
+        void Eat();
+    }
+
+    public abstract class DailyLife : ILife
+    {
+        public abstract void Breakfast();
+        public abstract void Lunch();
+        public abstract void Dinner();
+
+        public abstract int Number { get; set; }
+
+        public void Eat()
+        {
+            Breakfast();
+            Lunch();
+            Dinner();
+        }
+    }
+
+    public class JeanLife : DailyLife
+    {
+        public override int Number { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public override void Breakfast()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Dinner()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Lunch()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StartNewDay()
+        {
+            Eat();
+        }
+    }
+
+    class Practice
+    {
+    }
+}
